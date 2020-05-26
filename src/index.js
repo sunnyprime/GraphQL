9e58eb4ec6ff8ee16d35e689c6b0200635013d7c
unknown <kr.vivek.kr@gmail.com>
2020-05-26 10:17:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ApolloProvider} from 'react-apollo';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {createHttpLink} from 'apollo-link-http';
+import {ApolloClient,gql} from 'apollo-boost'
+
+const httpLink = createHttpLink ({
+  uri:'https://crwn-clothing.com/'
+})
+
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+client.query({
+  query: gql`
+  {
+    getCollectionsByTitle(title:"hats"){
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+  `
+}).then(res=>console.log(res))
 
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
